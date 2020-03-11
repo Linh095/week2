3257b0818fd02f8b1ba3fde732d9c25655cc3587
Linh095 <linh.nguyen.nvhl@gmail.com>
2020-03-11 17:18:53 +0000

    separated input to another file

--- d3/play.js
+++ d3/play.js
@@ -1,20 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-}
 
-const handleUserInput = (data) => {
-  if (data === "\x03") {
-    process.exit();
-  }
-};
 
 setupInput();
