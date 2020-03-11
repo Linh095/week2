6e14792b52eb50970cfc5ef923d33bb247698250
Linh095 <linh.nguyen.nvhl@gmail.com>
2020-03-11 17:13:00 +0000

    added and exit with control c

--- d3/play.js
+++ d3/play.js
@@ -1,3 +1,20 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = (data) => {
+  if (data === "\x03") {
+    process.exit();
+  }
+};
+
+setupInput();
